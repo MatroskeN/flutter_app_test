fedbaa5981c5909db152ca1af2b0a27a70297946
MatroskeN <44238649+MatroskeN@users.noreply.github.com>
2023-12-13 00:28:33 +0300

    randomize

---
+++ lib/counter.dart
@@ -0,0 +1,16 @@
+import 'package:anitex/anitex.dart';
+import 'package:flutter/material.dart';
+
+class Counter extends StatelessWidget {
+  const Counter(this.value, {Key? key}) : super(key: key);
+
+  final int value;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedText(
+      '$value',
+      style: Theme.of(context).textTheme.headlineMedium,
+    );
+  }
+}
